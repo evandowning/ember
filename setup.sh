42aaf51493c845e92de698c8c86bb999d47361ac
Evan Downing <2077950+evandowning@users.noreply.github.com>
2020-11-01 23:09:59 +0000

    add setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Download Anaconda3
+wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+
+# Run Anaconda setup
+bash ./Anaconda3-2020.07-Linux-x86_64.sh -b
+conda init
+source ~/.bashrc
+
+# Update Anaconda
+conda update conda -y
+
+# Setup Ember
+conda create -n ember python=3.6 anaconda -y
+conda activate ember
+conda config --add channels conda-forge
+conda install --file requirements_conda.txt -y
+python setup.py install
+
+conda deactivate
