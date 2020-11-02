3a782b186f96fc1e84038fabb9de7fa4790ea64d
Evan Downing <2077950+evandowning@users.noreply.github.com>
2020-11-02 05:45:40 +0000

    update conda setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
 
 # Run Anaconda setup
-bash ./Anaconda3-2020.07-Linux-x86_64.sh -b
+bash ./Anaconda3-2020.07-Linux-x86_64.sh -b -p /root/anaconda3/
+source /root/anaconda3/bin/activate
 conda init
 source ~/.bashrc
 
